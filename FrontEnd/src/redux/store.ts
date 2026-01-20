5cf2de8ae4ebc2be139edaa4de446fcf7ef0e1b6
sakthimurugesanr <sakthicareer001@gmail.com>
2026-01-20 12:34:06 +0530

    init login

--- FrontEnd/src/redux/store.ts
+++ FrontEnd/src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/AuthSlice";
+import loginSlice from "./features/LoginSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    login: loginSlice,
   },
 });
 
